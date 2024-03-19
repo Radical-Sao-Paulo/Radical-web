7763b99ded1e99f24eb8413527c9a2ebedbcbdc6
Mateus <mateus.saraiva09@gmail.com>
2024-03-19 02:43:09 -0300

    built navigation bar structure

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -1,4 +1,26 @@
 'use client'
+import {
+    Main,
+    Navbar,
+    Navbar_image_Logo,
+    Navbar_container_itens,
+    Navbar_itens
+} from './style'
+import RadicalLogo from '../../../public/favicon.png'
+
 export default function Home() {
-    return <div>hello</div>
+    return (
+        <Main>
+            <Navbar>
+                <Navbar_image_Logo src={RadicalLogo} />
+                <Navbar_container_itens>
+                    <Navbar_itens>1</Navbar_itens>
+                    <Navbar_itens>2</Navbar_itens>
+                    <Navbar_itens>3</Navbar_itens>
+                    <Navbar_itens>4</Navbar_itens>
+                    <Navbar_itens>5</Navbar_itens>
+                </Navbar_container_itens>
+            </Navbar>
+        </Main>
+    )
 }
--- src/app/Home/style.js
+++ src/app/Home/style.js
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 import Image from "next/image";
 
+export const Main = styled.main``
+
+export const Navbar = styled.nav``
+
+export const Navbar_image_Logo = styled(Image)``
+
+export const Navbar_container_itens = styled.ul``
+
+export const Navbar_itens = styled.li``
