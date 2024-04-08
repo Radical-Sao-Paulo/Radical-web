ec6caf79d5dbfa5d81c8dc5f704ee0e9ced3f088
Mateus <mateus.saraiva09@gmail.com>
2024-04-08 03:24:55 -0300

    Create first article

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -1,7 +1,15 @@
 'use client'
 import {
     Main,
-
+    Article_project,
+    Header_project,
+    Title_project,
+    Subtitle_project,
+    Section_description_project,
+    Label_description_project,
+    Barra_project,
+    Buttons_project,
+    Button_project
 } from './style'
 
 import Navbar from '../../components/navbar'
@@ -9,7 +17,24 @@ import Navbar from '../../components/navbar'
 export default function Home() {
     return (
         <Main>
+            {/* Component Navbar*/}
             <Navbar />
+
+            {/* Article main*/}
+            <Article_project>
+                <Header_project>
+                    <Title_project>Radical</Title_project>
+                    <Subtitle_project>São Paulo</Subtitle_project>
+                </Header_project>
+                <Section_description_project>
+                    <Label_description_project>O Projeto Radical São Paulo é um retiro espiritual cristão com duração de um final de semana em um sítio próximo à Aruja - SP, onde centenas de pessoas, famílias e igrejas estão sendo impactadas e desafiadas a tomarem atitudes que agradem à Deus no seu viver diário.</Label_description_project>
+                    <Barra_project></Barra_project>
+                </Section_description_project>
+                <Buttons_project>
+                    <Button_project>Inscreva-se</Button_project>
+                    <Button_project>Saiba Mais</Button_project>
+                </Buttons_project>
+            </Article_project>
         </Main>
     )
 }
--- src/app/Home/style.js
+++ src/app/Home/style.js
@@ -1,51 +1,76 @@
 import styled from "styled-components";
-import Image from "next/image";
 
 export const Main = styled.main`
-    height: 90vh;
+    height: 95vh;
     background-color: #f8002f;
     background-image: linear-gradient(326deg, #f8002f 0%, #000c14 74%);
 `
 
-export const Navbar = styled.nav`
-    height: 100px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
+export const Article_project = styled.article`
+    height: calc(100vh - 100px);
 `
 
-export const Navbar_image_Logo = styled(Image)`
-    width: 50px;
-    height: 50px;
-    background-color: #c60716;
+export const Header_project = styled.header`
+   display: flex;
+   justify-content: center;
+   flex-direction: column;
+   height: calc(50vh - 50px);
 `
 
-export const Navbar_container_itens = styled.ul`
-    display: flex;
+export const Title_project = styled.h1`
+   color: #ffffff;
+   font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+   font-size: 6rem;
+   padding-left: 100px;
+   animation: slide .7s ease-in-out;
 `
 
-export const Navbar_itens = styled.li`
-    color: aliceblue;
-    padding: 20px;
-    position: relative;
-    font-size: 15px;
-    animation: move-up .7s ease-out;
-    list-style-type: none;
-    cursor: pointer;
+export const Subtitle_project = styled.h2`
+   color: #c60716;
+   font-family: Impact, 'Arial Narrow Bold', sans-serif;
+   font-size: 3.5rem;
+   padding-left: 230px;
+   animation: slide 1s ease-in-out;
+`
 
-    &::after {
-        content: ' ';
-        width: 0%;
-        height: 2px;
-        background-color: #c60716;
-        left: 0;
-        bottom: 0;
-        position: absolute;
-        transition: 0.5s ease-in-out;
-    }
+export const Section_description_project = styled.section`
+   width: 600px;
+   text-align: left;
+   padding-left: 100px;
+`
 
-    &:hover::after {
-        width: 100%;
-    }
+export const Label_description_project = styled.label`
+   color: #ffffff;
+   position: relative;
+   animation: opacity .7s;
+`
+
+export const Barra_project = styled.div`
+   width: 50%;
+   height: 3px;
+   background-color: #c60716;
+   margin-top: 5px;
+`
+
+export const Buttons_project = styled.section`
+   padding-left: 100px;
+   margin-top: 10px;
+`
+
+export const Button_project = styled.button`
+   border: 1px solid #c60716;
+   background-color: transparent;
+   color: #ffffff;
+   padding: 10px;
+   border-radius: 20px;
+   font-family: Impact;
+   width: 200px;
+   font-size: 20px;
+   cursor: pointer;
+   transition: ease-in-out .3s;
+   margin: 5px;
+
+   &:hover {
+      background-color: rgb(198, 7, 22);
+   }
 `
